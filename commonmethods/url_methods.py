c502db0c03901c64a34513b78bb569e8b38aaa03
2014QING <2014qing@gmail.com>
2016-09-01 13:45:58 +0800

    add annotations to url_methods

--- commonmethods/url_methods.py
+++ commonmethods/url_methods.py
@@ -21,14 +21,25 @@ class UrlMethods():
         return get_rs
     def url_post_method(self,url,post_data):
         cookie=self.cookie
+        #post_data要求是dict类型
         data=urllib.parse.urlencode(post_data)
         data=data.encode('utf-8')
         req = urllib.request.Request(url)
         req.add_header('Cookie', cookie)
-        req_urlopen = urllib.request.urlopen(req,data)
-        f = req_urlopen.read().decode('utf-8')
+        req_post = urllib.request.urlopen(req,data)
+        f = req_post.read().decode('utf-8')
         post_rs = json.loads(f)
         return post_rs
+    #post方法传递的数据可以是dict，也可以是str，都需要encode
+    # def url_post_str_data_method(self,url,str_data):
+    #     cookie=self.cookie
+    #     req=urllib.request.Request(url,str_data.encode('utf-8','ignore'))
+    #     req.add_header('Cookie',cookie)
+    #     req_str_data=urllib.request.urlopen(req)
+    #     f=req_post_str.read().decode('utf-8')
+    #     str_rs=json.loads(f)
+    #     return str_rs
+    #上传图片
     def url_image_upload_method(self,url,image_data,headers):
         req=urllib.request.Request(url,image_data.encode('ISO-8859-1','ignore'))
         req.add_header('Content-Type' ,headers['Content-type'])
@@ -43,14 +54,19 @@ class UrlMethods():
             print(e.read())
 
 
-
+    #上传图片前生成multipart/form-data数据
     def url_multipart_formdata(self,files,fields,boundary=None):
+    '''Encode dict of form fields and dict of files as multipart/form-data.
+    Return tuple of (body_string, headers_dict). Each value in files is a dict
+    with required keys 'filename' and 'content', and optional 'mimetype' (if
+    not specified, tries to guess mime type or uses 'application/octet-stream').'''
         #大小写字母string.ascii_letters:The concatenation of the ascii_lowercase and ascii_uppercase constants described below. This value is not locale-dependent.
         #数字string.digits:The string '0123456789'.
         _BOUNDARY_CHARS=string.digits+string.ascii_letters
         #str.replace(old, new[, max])replace() 方法把字符串中的 old（旧字符串） 替换成 new(新字符串)，如果指定第三个参数max，则替换不超过 max 次
         def escape_quote(s):
             return s.replace('"','\\"')
+
         if boundary is None:
             #random.choice(seq) Return a random element from the non-empty sequence seq. If seq is empty, raises IndexError.
             boundary=''.join(random.choice(_BOUNDARY_CHARS) for i in range(16))
@@ -69,6 +85,7 @@ class UrlMethods():
                 '',
                 value['content'].read().decode('ISO-8859-1'),
             ))
+
         for name,value in fields.items():
             lines.extend((
                 '------------WebKitFormBoundary{0}'.format(boundary),
@@ -80,12 +97,16 @@ class UrlMethods():
             '------WebKitFormBoundary{0}--'.format(boundary),
             '',
         ))
+
         body='\r\n'.join(lines)
+
         headers={
             'Content-type':'multipart/form-data; boundary=----WebKitFormBoundary{0}'.format(boundary),
             'Content-Length':str(len(body)),
         }
+        
         return(body,headers)
+
 
 # if __name__=='__main__':
 #     um=UrlMethods()
